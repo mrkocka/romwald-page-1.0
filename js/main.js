09d325d4bbddbb81ce10cb6f0bb170da01aa42e5
Mr. Joe <radics.pepe@gmail.com>
2023-02-13 21:21:10 +0100

    van már vide, és popup ablak

--- js/main.js
+++ js/main.js
@@ -15,3 +15,19 @@ function Hmenu() {
     icon.classList.toggle("bi-x-lg");
   }
 }
+
+//--------------------------------------Pop Up ---------------------------------------
+
+let close = document.getElementById("close-js");
+
+window.addEventListener("load", function () {
+  this.setTimeout(function open(event) {
+    document.getElementById("popup-js").classList.toggle("popup-container");
+  }, 2000);
+});
+
+close.addEventListener("click", cloesPopUp);
+
+function cloesPopUp() {
+  document.getElementById("popup-js").classList.toggle("popup-container");
+}
